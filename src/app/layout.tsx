242e87ff7a4f03373d7f5d29f3248749ddc83f66
Przemek <przemek.lewtak@heywood.co.uk>
2023-12-18 13:35:48 +0000

    Fixed errors lint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,64 +1,60 @@
 import type { Metadata } from "next";
 import { Poppins, Rubik } from "next/font/google";
+import { siteConfig } from "./page";
 import { cn } from "@/lib/utils";
 import "./globals.css";
-import { siteConfig } from "./page";
 
 const poppins = Poppins({
-  subsets: ["latin"],
-  weight: "400",
-  variable: "--font-poppins",
+	subsets: ["latin"],
+	weight: "400",
+	variable: "--font-poppins",
 });
 const rubik = Rubik({
-  subsets: ["latin"],
-  weight: "600",
-  variable: "--font-rubik",
+	subsets: ["latin"],
+	weight: "600",
+	variable: "--font-rubik",
 });
 
 export const metadata: Metadata = {
-  title: {
-    default: siteConfig.name,
-    template: `%s - Software Engineer`,
-  },
-  description: siteConfig.description,
-  keywords: [
-    "portfolio",
-    "web developer",
-    "web",
-    "web dev",
-    "developer",
-    "PROGRAMMER ",
-    "programmer ",
-    "Przemek Lewtak",
-  ],
-  authors: [
-    {
-      name: "Przemek Lewtak",
-      url: "https://github.com/matchmakers69",
-    },
-  ],
-  creator: "Przemek Lewtak",
+	title: {
+		default: siteConfig.name,
+		template: `%s - Software Engineer`,
+	},
+	description: siteConfig.description,
+	keywords: [
+		"portfolio",
+		"web developer",
+		"web",
+		"web dev",
+		"developer",
+		"PROGRAMMER ",
+		"programmer ",
+		"Przemek Lewtak",
+	],
+	authors: [
+		{
+			name: "Przemek Lewtak",
+			url: "https://github.com/matchmakers69",
+		},
+	],
+	creator: "Przemek Lewtak",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body className={`${poppins.variable} ${rubik.variable}`}>
-        <main
-          className={cn(
-            "bg-transparent bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] max-sm:pt-20",
-            { "bg-white": "#E6E7EB" },
-          )}
-        >
-          {/* NAVBAR ->  */}
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<html lang="en">
+			<body className={`${poppins.variable} ${rubik.variable}`}>
+				<main
+					className={cn(
+						"bg-transparent bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] max-sm:pt-20",
+						{ "bg-white": "#E6E7EB" },
+					)}
+				>
+					{/* NAVBAR ->  */}
 
-          {children}
-        </main>
-      </body>
-    </html>
-  );
+					{children}
+				</main>
+			</body>
+		</html>
+	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
 export const siteConfig = {
   name: "Przemek Lewtak",
   description: "I am a Passionate Software Developer",
-}
+};
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen">
-    Hello
-    </main>
-  )
+  return <main className="flex min-h-screen">Hello</main>;
 }
